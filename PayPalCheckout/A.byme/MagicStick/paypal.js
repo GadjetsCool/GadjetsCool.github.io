bf8ca516a21eb5a7ad53c78daba185f29f9617f2
Gadget <93807296+GadjetsCool@users.noreply.github.com>
2021-11-07 18:30:52 +0100

    Update paypal.js

--- PayPalCheckout/A.byme/MagicStick/paypal.js
+++ PayPalCheckout/A.byme/MagicStick/paypal.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units:[{
               amount: {
-                  value:'0.01'
+                  value:'10.99'
               }
             }]
         })
