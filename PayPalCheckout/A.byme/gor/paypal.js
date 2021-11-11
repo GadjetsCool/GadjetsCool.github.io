945eb9105cbb0b624c67a12699b01375cbca9bc3
Gadget <93807296+GadjetsCool@users.noreply.github.com>
2021-11-11 01:12:28 +0100

    Add files via upload

---
+++ PayPalCheckout/A.byme/gor/paypal.js
@@ -0,0 +1,22 @@
+paypal.Buttons({
+    style : {
+        color: 'blue'
+    },
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units:[{
+              amount: {
+                  value:'9.99'
+              }
+            }]
+        })
+    },
+    onApprove: function(data, actions) {
+        // This function captures the funds from the transaction.
+        return actions.order.capture().then(function(details) {
+            console.log(details)
+            alert('Transaction completed by ' + details.payer.name.given_name);
+          window.location.replace("success.html")
+        });
+    }
+}).render('#paypal-payment-button');
