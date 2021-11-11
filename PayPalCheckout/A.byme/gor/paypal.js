10f16aa031b04d21f07021810f4fef23db183f48
Gadget <93807296+GadjetsCool@users.noreply.github.com>
2021-11-11 23:39:20 +0100

    Add files via upload

--- PayPalCheckout/A.byme/gor/paypal.js
+++ PayPalCheckout/A.byme/gor/paypal.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units:[{
               amount: {
-                  value:'9.99'
+                  value:'9.90'
               }
             }]
         })
