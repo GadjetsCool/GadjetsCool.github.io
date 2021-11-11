4dfffc15322ed292297105330d02de3e6220b33a
Gadget <93807296+GadjetsCool@users.noreply.github.com>
2021-11-11 23:35:54 +0100

    Add files via upload

--- PayPalCheckout/A.byme/coque/paypal.js
+++ PayPalCheckout/A.byme/coque/paypal.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units:[{
               amount: {
-                  value:'13.99'
+                  value:'8.77'
               }
             }]
         })
